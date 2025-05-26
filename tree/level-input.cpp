9e6f9645bf914773a265ae3f4b3a07472312b514
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-05-26 23:10:48 +0530

    RPF and some tree operations

---
+++ tree/level-input.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int val;
+    struct node* left;
+    struct node* right;
+};
+typedef struct node* bu;
+
+bu create(int x){
+    bu temp=(bu)malloc(sizeof(struct node));
+    temp->val=x;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+
+bu add(bu root,int arr[],int n){
+    if(n==0) return NULL;
+
+    bu queue[1000];
+    int front=0,rear=0;
+
+    root=create(arr[0]);
+    queue[rear++]=root;
+
+    int i=1;
+    while(i<n){
+        bu curr=queue[front++];
+        if(i<n){
+            curr->left=create(arr[i++]);
+            queue[rear++]=curr->left;
+        }if(i<n){
+            curr->right=create(arr[i++]);
+            queue[rear++]=curr->right;
+        }
+    }
+    return root;
+}
+
+void inorder(bu root){
+    if(root){
+        inorder(root->left);
+        printf("%d ",root->val);
+        inorder(root->right);
+    }
+}
+
+void postorder(bu root){
+    if(root){
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ",root->val);
+    }
+}
+
+int main(){
+    int arr[1000];
+    int n=0,data;
+    
+    while(true){
+        scanf("%d",&data);
+        if(data==-1) break;
+        arr[n++]=data;
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }printf("\n");
+
+    bu root=NULL;
+
+    root=add(root,arr,n);
+
+    postorder(root);
+}
